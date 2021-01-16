ff5b05761ca69699ac4eadc462c261f069d5b9c6
himynameisdave <dave@fossa.com>
2021-01-16 12:04:05 -0800

    📦 Package upgrades, update rollup config
    Closes #4

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,7 @@ import svelte from 'rollup-plugin-svelte';
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import livereload from 'rollup-plugin-livereload';
+import css from 'rollup-plugin-css-only';
 import json from 'rollup-plugin-json';
 import { terser } from 'rollup-plugin-terser';
 import sveltePreprocess from 'svelte-preprocess';
@@ -11,7 +12,7 @@ const production = !process.env.ROLLUP_WATCH;
 
 function serve() {
 	let server;
-	
+
 	function toExit() {
 		if (server) server.kill(0);
 	}
@@ -40,15 +41,15 @@ export default {
 	},
 	plugins: [
 		svelte({
-			// enable run-time checks when not in production
-			dev: !production,
-			// we'll extract any component CSS out into
-			// a separate file - better for performance
-			css: css => {
-				css.write('bundle.css');
+			compilerOptions: {
+				// enable run-time checks when not in production
+				dev: !production,
 			},
 			preprocess: sveltePreprocess(),
 		}),
+		// we'll extract any component CSS out into
+		// a separate file - better for performance
+		css({ output: 'bundle.css' }),
 
 		// If you have external dependencies installed from
 		// npm, you'll most likely need these plugins. In
@@ -63,7 +64,7 @@ export default {
 		//	Add JSON support
 		json({
 			compact: true,
-		})
+		}),
 		typescript({
 			sourceMap: !production,
 			inlineSources: !production
