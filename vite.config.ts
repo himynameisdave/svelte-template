59ca62252c20d015c68476fe4bb402eab33e09dd
himynameisdave <dave@fossa.com>
2023-02-05 17:20:18 -0800

    🐶 Add husky pre-commit hooks

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import type { UserConfig } from 'vite';
+import type { InlineConfig } from 'vitest';
 
-const config: UserConfig = {
-	plugins: [sveltekit()],
-	test: {
-		include: ['src/**/*.{test,spec}.{js,ts}']
-	}
+const config: UserConfig & { test: InlineConfig } = {
+  plugins: [sveltekit()],
+  test: {
+    include: ['src/**/*.{test,spec}.{js,ts}']
+  },
 };
 
 export default config;
